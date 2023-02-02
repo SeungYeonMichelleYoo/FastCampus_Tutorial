68fa2c66d5d3e51094622caf85b2e910cb008423
michelle <lune111200@gmail.com>
2023-02-02 17:41:04 +0900

    UI: TableVC & TableViewCell

---
+++ GithubPractice/RepositoryListCell.swift
@@ -0,0 +1,32 @@
+//
+//  RepositoryListCell.swift
+//  GithubPractice
+//
+//  Created by SeungYeon Yoo on 2023/02/02.
+//
+
+import UIKit
+import SnapKit
+
+class RepositoryListCell: UITableViewCell {
+    var repository: String?
+    
+    let nameLabel = UILabel()
+    let descriptionLabel = UILabel()
+    let starImageView = UIImageView()
+    let starLabel = UILabel()
+    let languageLabel = UILabel()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        [
+            nameLabel, descriptionLabel,
+            starImageView, starLabel, languageLabel
+        ].forEach {
+            contentView.addSubview($0)
+        }
+        
+        
+    }
+}
---
+++ GithubPractice/RepositoryListViewController.swift
@@ -0,0 +1,44 @@
+//
+//  RepositoryListViewController.swift
+//  GithubPractice
+//
+//  Created by SeungYeon Yoo on 2023/02/02.
+//
+
+import UIKit
+
+class RepositoryListViewController: UITableViewController {
+    private let organization = "Apple"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = organization + "Repositories"
+        
+        self.refreshControl = UIRefreshControl() //당겨서 새로고침 할 때마다 api call 하는 앱
+        let refreshControl = self.refreshControl!
+        refreshControl.backgroundColor = .white
+        refreshControl.tintColor = .darkGray
+        refreshControl.attributedTitle = NSAttributedString(string: "당겨서 새로고침") //인디케이터 아래에 있는 글자
+        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        
+        tableView.register(RepositoryListCell.self, forCellReuseIdentifier: "RepositoryListCell")
+        tableView.rowHeight = 140
+    }
+    
+    @objc func refresh() {
+        
+    }
+}
+
+//UITableView DataSource Delegate
+extension RepositoryListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "RepositoryListCell", for: indexPath) as? RepositoryListCell else { return UITableViewCell() }
+        return cell
+    }
+}
