5a5ba52ce0faa5d9bb89d634c9d90a6abd55166c
michelle <lune111200@gmail.com>
2023-06-20 17:35:03 +0900

    Snapkit UI Setting

---
+++ Translate/Translate/UIColor+.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+.swift
+//  Translate
+//
+//  Created by SeungYeon Yoo on 2023/06/18.
+//
+
+import UIKit
+
+extension UIColor {
+    static var mainTintColor = UIColor.systemPink
+}
